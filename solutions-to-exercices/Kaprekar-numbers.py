cbb57c6633eb45bb86ac6c01d3dc5a19ca6fa8ae
chrplr <christophe@pallier.org>
2018-12-06 10:21:11 +0100

    projects updated

--- solutions-to-exercices/Kaprekar-numbers.py
+++ solutions-to-exercices/Kaprekar-numbers.py
@@ -4,17 +4,41 @@
 """ List Kaprekar numbers, i.e., positive numbers n such that n = q+r and n^2 = q*10^m+r, for some m >= 1, q >= 0 and 0 <= r < 10^m, with n != 10^a, a >= 1.  (see http://oeis.org/A006886).
 """
 
-def form_number(list_of_digits):
+
+def number_to_symbols(number, base=10):
+    """Return the representation of 'number' in base 'base'.
+    """ 
+    assert number == int(number)  # check that number is an integer
+    n = number
+    symbols = []
+    while n > 0:
+        val = n % base
+        if val < 9:
+            valc =str(val)
+        else:
+            valc = chr(ord('A') + val - 10)  # map digits > 9 onto [A-Z]
+        symbols.append(valc)
+        n = n // base
+    symbols.reverse()
+    return symbols
+
+
+def form_number(list_of_digits, base=10):
     """ returns the number represented by the digits passed as argument. """
     number = 0
     for digit in list_of_digits:
-        number = number * 10 + digit
+        c = ord(digit)
+        if (c >= ord('0')) and (c <= ord('9')):
+            val = c - ord('0')
+        else:
+            val = 10 + c - ord('A')
+        number = number * base + val
     return number
 
 
 def sums_of_parts(n):
     """ returns the sums obtained by splitting the decimal representation of 'n' in two and adding the resulting numbers """
-    digits = [int(c) for c in str(n)]  # obtain the digits from the decimal representation of n
+    digits = number_to_symbols(n) 
     sums = []
     for pos in range(len(digits)):
         left = form_number(digits[:pos])
