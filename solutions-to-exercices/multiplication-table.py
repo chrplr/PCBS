b8fcf34c8750000cdd4da4aff8886a6b993bb402
chrplr <christophe@pallier.org>
2019-03-21 10:54:56 +0100

    clean formating on 100 in mutliplication table

--- solutions-to-exercices/multiplication-table.py
+++ solutions-to-exercices/multiplication-table.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# Time-stamp: <2018-11-04 10:40:58 cp983411>
+# Time-stamp: <2019-03-21 10:44:22 christophe@pallier.org>
 
 """ print the multiplication tables """
 
@@ -7,9 +7,9 @@ MAX = 10
 
 # display 1
 for i in range(1, MAX + 1):
-    print(end='\n')  # skip a line
+    print(f'\nTable of {i:3d}:')
     for j in range(1, MAX + 1):
-        print(f'{i:2} x {j:2} = {i*j:2}')
+        print(f'{i:2} x {j:2} = {i*j:3}')
 
 
 # display 2
