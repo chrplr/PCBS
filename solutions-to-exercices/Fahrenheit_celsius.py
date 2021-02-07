02edee036c9db4870e32fc09e931597aa0b20b48
Christophe Pallier <christophe@pallier.org>
2021-02-07 21:03:34 +0100

    new version of temperature conversion

--- solutions-to-exercices/Fahrenheit_celsius.py
+++ solutions-to-exercices/Fahrenheit_celsius.py
@@ -1,11 +1,91 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python3
 """
-Created on Thu Oct 17 11:26:41 2019
+Temperature unit converter.
 
-@author: install
+@author: christophe@pallier.org
 """
 
 import sys
+
+def F_to_C(f):
+    """Conversion °F -> °C
+
+    Args:
+        f (float): temperature in °F
+
+    Returns:
+        float: temperature in °C
+    """
+    return (f - 32) * 5 / 9
+
+
+def C_to_F(c):
+    """Conversion °C -> °F
+
+    Args:
+        c (float): temperature in °C
+
+    Returns:
+        float: temperature in °F
+    """
+
+    return (c * 9 / 5) + 32
+
+
+def str2temp(string):
+    """Converts a string into a temperature (value, unit).
+
+    Args:
+        string (str): string representation of a temperature
+
+    Returns:
+        a couple (float, char) where float is the temperature value and char is 'C' or 'F'
+    """
+    string = string.strip() # remove trailing whitespaces
+    value = string[:-1]  # get the first characters but last
+    try:
+       val = float(value)
+    except ValueError:
+        try:
+            val = float(string[:-2])  # maybe there is a '°' before the unit
+        except ValueError:
+            raise ValueError("The first n-1 characters of %s are not a float" % string)
+
+    unit = string[-1].upper()  # get the last character
+    if unit not in "CF":
+        raise ValueError("unknown unit %s should end in C or F" % string)
+    return (val, unit)
+
+
+
+def help():
+    print('Usage: %s', sys.argv[0])
+    print('Convert a list of temperatures from Celsius to Farenheit or vice-versa.')
+    print('Each line on the standard input should contain one temperature of the form xxx{F|C} ')
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        help()
+        sys.exit()
+
+    for line in sys.stdin:
+        value, unit = str2temp(line)
+        if unit == 'C':
+            print("%.1f°C = %.1f°F" % (value, C_to_F(value)))
+        elif unit == 'F':
+            print("%.1f°F = %.1f°C" % (value, F_to_C(value)))
+        else:
+            print("Unknown unit : %s (should be 'C' or 'F')" % unit)
+
+
+
+
+
+
+
 if sys.argv[1]=="F":
     temp = (int(sys.argv[2]) - 32)*5/9
     print(temp, " °C")
