a5a99fe81153ec92b0c21261ec9ae8d2f842ea7a
Christophe Pallier <christophe@pallier.org>
2021-04-15 10:34:44 +0200

    cleaned create_stroop_card.py

--- experiments/stroop/create_stroop_cards.py
+++ experiments/stroop/create_stroop_cards.py
@@ -1,20 +1,56 @@
 #! /usr/bin/env python
-# Time-stamp: <2021-04-05 20:26:07 christophe@pallier.org>
+# Time-stamp: <2021-04-15 10:23:31 christophe@pallier.org>
 # Author: Christophe Pallier (see http://www.pallier.org)
 
 import random
 import pygame
 
+RED = pygame.Color('red')
+YELLOW = pygame.Color('yellow')
+GREEN = pygame.Color('green')
+WHITE = pygame.Color('white')
+BLACK = pygame.Color("black")
 
-def share_item_in_same_position(list1, list2):
-    return any([i == j  for (i, j) in zip(list1, list2)])
+
+french = (('rouge', RED), ('blanc', WHITE), ('jaune', YELLOW), ('vert', GREEN))
+english = (('red', RED), ('white', WHITE), ('yellow', YELLOW), ('green', GREEN))
+basque = (('gorri', RED), ('zuri', WHITE), ('horri', YELLOW), ('berde', GREEN))
 
 
-def create_stroop_card(items, font, background_color=pygame.Color('black')):
+def match(word, color, items):
+    return (word, color) in items
+
+
+def generate_array(n_row, n_col, items):
+    """ returns an array (list of lists) of n rows, p columns
+        containing pairs (word, color)
+        such that there is no repetition of color on line,
+        nor in successive rows.
+    """
+    words = [x for x, y in items]
+    colors = [y for x, y in items]
+    array = []
+
+    # we fill row by row, checking compatibility with previous row
+    previous_row = [""] * n_col  # colors on the current row
+    for i_row in range(n_row):
+        perm1 = random.sample(words, n_col)
+        perm2 = random.sample(colors, n_col)
+        while any([match(word, color, items) for word, color in zip(perm1, perm2)]) or \
+              any([col1 == col2 for col1, col2 in zip(previous_row, perm2)]):
+            perm2 = random.sample(colors, n_col)
+
+        array.append(list(zip(perm1, perm2)))
+        previous_row = perm2
+
+    return array
+
+
+def create_stroop_card(array, font, background_color=pygame.Color('black')):
     """Create an image representing a Stroop card.
 
     Argument:
-       items              :   a dictionary word->color, e.g. {'rouge':'red', 'bleu':'blue', 'vert':'green', 'jaune':'yellow'}
+       items              :   a table (list of lists) of pairs (string, color)
        font               :   a pygame font object
        background_color 
 
@@ -22,44 +58,26 @@ def create_stroop_card(items, font, background_color=pygame.Color('black')):
        a pygame surface
     """
 
-    words = list(items.keys())
-    colors = list(items.values())
-    n = len(items)
-
     W, H = 800, 600
     margin = 20
-
     grid = pygame.Surface((W, H))
     grid.fill(background_color)
 
-    label = pygame.Surface((W, H))
-
-    previous_row = [-1, -1, -1, -1]  # colors on the current row
-    for i_row in range(n):
-        i_word = random.sample(range(n), n)
-        i_color = random.sample(range(n), n)
-        while share_item_in_same_position(i_word, i_color) or \
-              share_item_in_same_position(previous_row, i_color):
-            i_color = random.sample(range(n), n)
-
-        previous_row = i_color
-
-        # fill the current row
-        y = i_row * H / n + margin
-
-        for i in range(n):
-            x = i * W / n + margin
-            label.fill(background_color)
-            label.blit(font.render(words[i_word[i]],
-                                   True, pygame.color.Color(colors[i_color[i]])), (0, 0))
-            grid.blit(label, (x, y))
+    n_row = len(array)
+    n_col = len(array[0])
+    for i_row, row in enumerate(array):
+        for i_col, pair in enumerate(row):
+            word, color = pair
+            grid.blit(font.render(word, True, color),
+                      (i_col * W / n_col + margin, i_row * H / n_row + margin))
     return grid
 
 
 def create_cards(items, font, ncards, template):
-    for i in range(ncards):
-        grid = create_stroop_card(items, font, pygame.Color('black'))
-        pygame.image.save(grid, template % (i))
+    for i in range(1, ncards + 1):
+        word_color_array = generate_array(4, 4, items)
+        grid = create_stroop_card(word_color_array, font, pygame.Color('black'))
+        pygame.image.save(grid, template % i)
 
 
 if __name__ == "__main__":
@@ -69,20 +87,8 @@ if __name__ == "__main__":
     font_size = 44
     font = pygame.font.Font(font, font_size)
 
-    create_cards({'rouge': 'red',
-                  'blanc': 'white',
-                  'vert': 'green',
-                  'jaune': 'yellow'},
-                 font, 4, "Stroop_French_%02d.png")
-    create_cards({'red': 'red',
-                  'white': 'white',
-                  'green': 'green',
-                  'yellow': 'yellow'},
-                 font, 4, "Stroop_English_%02d.png")
-    create_cards({'gorri': 'red',
-                  'zuri': 'white',
-                  'berde': 'green',
-                  'horri': 'yellow'},
-                 font, 4, "Stroop_Basque_%02d.png")
+    create_cards(french, font, 4, "Stroop_French_%02d.png")
+    create_cards(english, font, 4, "Stroop_English_%02d.png")
+    create_cards(basque, font, 4, "Stroop_Basque_%02d.png")
 
     pygame.quit()
