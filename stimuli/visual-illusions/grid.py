639483331a0e016c0a615eb6a9f7eb7ae9fc93fb
Christophe Pallier <christophe@pallier.org>
2024-02-14 10:07:37 +0100

    added color vars to grid.py

--- stimuli/visual-illusions/grid.py
+++ stimuli/visual-illusions/grid.py
@@ -13,6 +13,8 @@ n_rows, n_cols = 10, 10
 cell_side_length = 50
 space_between_cells = 10
 margin = 30  # borders size
+square_color = BLACK
+background_color = WHITE
 
 # graphic window cell_side_length
 W = n_cols * cell_side_length + (n_cols - 1) * space_between_cells + 2 * margin
@@ -23,7 +25,7 @@ pygame.init()
 screen = pygame.display.set_mode((W, H), pygame.DOUBLEBUF)
 pygame.display.set_caption('Herman Grid')
 
-screen.fill(WHITE)
+screen.fill(background_color)
 
 # construction of the grid
 for i_row in range(n_rows):
@@ -31,7 +33,7 @@ for i_row in range(n_rows):
         x_left = margin + i_col * (cell_side_length + space_between_cells)
         y_top = margin + i_row * (cell_side_length + space_between_cells)
         pygame.draw.rect(screen,
-                         BLACK,
+                         square_color,
                          (x_left, y_top, cell_side_length, cell_side_length))
 
 # display the backbuffer
